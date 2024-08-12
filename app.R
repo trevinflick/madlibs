d7cdccf9a3412421e546c73d9978046b57fdf275
Trevin Flickinger <twflick@gmail.com>
2024-08-12 14:06:00 -0700

    add logging

--- app.R
+++ app.R
@@ -1,10 +1,17 @@
 library(shiny)
 
 generate_story <- function(noun, verb, adjective, adverb) {
-  glue::glue("
+  story <- glue::glue("
     Once upon a time, there was a {adjective} {noun} who loved to
     {verb} {adverb}. It was the funniest thing ever!
   ")
+  
+  cat(strrep("-", 100), "\n", file = stderr())
+  cat("🐈\n", file = stderr())
+  cat(strrep("-", 100), "\n", file = stderr())
+  cat(story, file = stderr())
+  
+  story
 }
 
 ui <- fluidPage(
